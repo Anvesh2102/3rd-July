dfbc899797e87712545712df76315fba179797c5
Anvesh2102 <anveshkumaranthati2102@gmail.com>
2025-07-04 09:35:24 +0530

    AJIO cloning

---
+++ ajio.js
@@ -0,0 +1,9 @@
+// let hov=document.getElementsByClassName("sale")
+//     hov.addEventListener("mousedown",function(){
+//         hov.style.backgroundColor="coral";
+//         hov.style.border="2px solid black";
+//     });
+//     hov.addEventListener("mouseup",function(){
+//         hov.style.backgroundColor="azure";
+//         hov.style.border="0px";
+//     });
